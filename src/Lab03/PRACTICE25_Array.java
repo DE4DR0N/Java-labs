dbdf0bd384de79a1d4b46f1aed7c042546eef5de
Eugene Gedrevich <eugene.gedrevich@outlook.com>
2023-09-19 12:49:25 +0300

    Added tasks for the Lab03

---
+++ src/Lab03/PRACTICE25_Array.java
@@ -0,0 +1,50 @@
+/*
+Задание
+Изучите и законспектируйте теорию:
+1. Массивы в java
+Повторите выполнение данного примера.
+Разберитесь в решаемой задаче.
+Самостоятельно:
+Самостоятельно задать массив из от 10 до 30 элементов и инициализировать
+Написать метод сортировки массива по возрастанию (убыванию)
+Написать javadoc комментарий к методу
+Контрольные вопросы (ответы также включить в отчет)
+1. Как объявляются массивы?
+2. Какие существуют варианты объявления массивов?
+*/
+package Lab03;
+/**
+ * демонстрация массивов
+ *
+ * @author V.V. Sidorik
+ * @version 1.2 14.02.2017
+ */
+public class PRACTICE25_Array {
+    public static void main(String[] args) {
+        int[] numbers;
+        char[] letters, symbols;
+        long bigNumbers[];
+        String[] countries;
+        numbers = new int[3];
+        String[] currencies = new String[3];
+        numbers[0] = 100;
+        numbers[1] = 200;
+        numbers[2] = 300;
+        int[] newNumbers = {1, 2, 3};
+//Трассировка метода
+        int[] list = {1, 4, 4, 2, 5, -3, 6, 2};
+        int i = linearSearch(list, 4); // returns 1
+        int j = linearSearch(list, -4); // returns -1
+        int k = linearSearch(list, -3); // returns 5
+        System.out.println(i + " " + j + " " + k);
+    }
+    /**
+     * метод поиска key в списке
+     */
+    public static int linearSearch(int[] list, int key) {
+        for (int i = 0; i < list.length; i++)
+            if (key == list[i])
+                return i;
+        return -1;
+    }
+}
---
+++ src/Lab03/PRACTICE26_WindChillTemperature.java
@@ -0,0 +1,13 @@
+/*
+Задание
+Напишите код для расчет эффективной температуры воздуха в зависимости от
+температуры, скорости ветра и влажности. Для расчета используйте данные по
+ссылке или из других интернет источников.
+https://ggf.tsu.ru/content/faculty/structure/chair/meteorology/publications/%D0%
+9A%D0%BB%D0%B8%D0%BC%D0%B0%D1%82%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D1%8F/text/149.ht
+ml
+*/
+package Lab03;
+
+public class PRACTICE26_WindChillTemperature {
+}
