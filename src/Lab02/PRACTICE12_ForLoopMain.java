c3cff0e693af710b42ba32701669f002091a07fb
Eugene Gedrevich <eugene.gedrevich@outlook.com>
2023-09-20 10:15:12 +0300

    hotfix

--- src/Lab02/PRACTICE12_ForLoopMain.java
+++ src/Lab02/PRACTICE12_ForLoopMain.java
@@ -2,7 +2,8 @@ package Lab02;
 
 public class PRACTICE12_ForLoopMain {
     public static void main(String[] args){
-        for (int i = 0, j = 200; i < 100 || j < 500;i++, j++){
+        for (int i = 0, j = 200; j <= 500; i++, j++){
+            if (i == 100) i = 0;
             double z = i + j;
             if (z % 22 == 0){
                 System.out.printf("%d + %d = %.0f \n", i, j, z);
