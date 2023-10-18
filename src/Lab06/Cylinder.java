d439a390d9c468183767fc06c053db5ff12cd32f
DE4DR0N <eugene.gedrevich@outlook.com>
2023-10-18 13:01:18 +0300

    init

---
+++ src/Lab06/Cylinder.java
@@ -0,0 +1,40 @@
+package Lab06;
+import Lab05.Circle;
+
+/**
+ * Класс Cylinder является подклассом Circle
+ *
+ * @author Gedrevich E.A.
+ * @version 1.0 18.10.2023
+ */
+public class Cylinder extends Circle {
+    private double height; //Private member variable
+
+    public Cylinder() { // Constructor 1
+        super(); //выхов конструктора super класса
+        height = 1.0;
+    }
+    public Cylinder(double radius, double height) { // Constructor 2
+        super(radius);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override // Переопределяем метод getArea родительского класса
+    public double getArea() {
+        return 2 * Math.PI * getRadius() * height + 2 * super.getArea();
+    }
+    public double getVolume() {
+        return super.getArea() * height;
+    }
+    @Override // Переопределяем метод toString родительского класса
+    public String toString() {
+        return "Цилиндр: радиус = " + getRadius() + " высота = " + height;
+    }
+}
